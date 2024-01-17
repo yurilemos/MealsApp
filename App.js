96a55fbbb988387b2ee0fcf2e40ceaf6c773bdcc
Yuri Lemos <yuri.lemos@parafernalia.net.br>
2024-01-17 18:37:45 -0300

    wip meals app

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet } from "react-native";
+import CategoriesScreen from "./screens/CaregoriesScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar styles="light" />
+      <CategoriesScreen />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
